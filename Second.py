b53fd5ff30a4d8c2780a9f539abfdeb84563ea17
Raj Majhi <rajm46921@gmail.com>
2026-01-14 20:08:31 +0530

    Add Second.py with built-in functions and variables
    Introduced a new Python file containing explanations of built-in functions and variable naming rules.

---
+++ Second.py
@@ -0,0 +1,70 @@
+#Built in function
+"""
+In Python we have lots of built-in functions. Built-in functions are globally available for your use that 
+mean you can make use of the built-in functions without importing or configuring. 
+Some of the most commonly used Python built-in functions are the following: 
+print(), len(), type(), int(), float(), str(),
+input(), list(), dict(), min(), max(), sum(), 
+sorted(), open(), file(), help(), and dir().
+"""
+
+>>> print("hello")
+hello
+>>> min([10,20,2,30])
+2
+>>> max([30,20,30,50,50])
+50
+>>> sum([28,23,43,54])
+148
+>>> len('hellloooo')
+9
+>>> type(5)
+<class 'int'>
+>>> type("hxp")
+<class 'str'>
+>>> type(1.0)
+<class 'float'>
+>>> int('10')
+10
+>>> float(10)
+10.0
+>>> input("Enter your first name:")
+Enter your first name:Raj
+'Raj'
+
+#Python Variable 
+''' 
+A variable refers to a memory address in which data is stored.
+A variable can have a short name (like x, y, z), but a more 
+descriptive name (firstname, lastname, age, country) is highly recommended.
+'''
+# python variable naming rules
+"""
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (firstname, Firstname, FirstName and FIRSTNAME) are different variables)
+
+VALID:-
+firstname
+lastname
+age
+country
+city
+first_name
+last_name
+capital_city
+_if # if we want to use reserved word as a variable
+year_2021
+year2021
+current_year_2021
+birth_year
+num1
+num2
+INVALID:-
+first-name
+first@name
+first$name
+num-1
+1num
+"""
