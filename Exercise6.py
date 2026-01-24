064d18c694fc5435267db597bdadeabd5e78b08e
Raj Majhi <rajm46921@gmail.com>
2026-01-24 23:43:43 +0530

    Update Exercise6.py

--- Exercise6.py
+++ Exercise6.py
@@ -1,1 +1,21 @@
+#Function
+
+LEVEL 1
+
+def add_two_numbers():
+    a=5
+    b=10
+    sum=a+b
+    print('The sum of a & b :',sum)
+add_two_numbers()
+>>>The sum of a & b : 15
+
+def area_of_circle():
+    PI=3.14
+    r=int(input("Enter radius :"))
+    area = PI*r**2
+    print("area:",area)
+area_of_circle()
+>>>Enter radius :5
+area: 78.5
 
